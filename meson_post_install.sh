795b183eb58d86d7f99cc21c76fe5eb29cc14ac0
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-03-18 11:53:43 +0100

    Fix installation permissions (#11)
    * Update meson_post_install.sh
    
    * fix installation permission

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -6,5 +6,3 @@ gtk-update-icon-cache -f -t ${DESTDIR}/${MESON_INSTALL_PREFIX}/share/icons/hicol
 # Install new schemas
 glib-compile-schemas ${DESTDIR}/${MESON_INSTALL_PREFIX}/share/glib-2.0/schemas/
 
-# Fix binary permissions
-chmod +x  ${DESTDIR}/${MESON_INSTALL_PREFIX}/bin/icon-requests
